ad49fd6d7540551efa9e3338675d0dce2a737119
CucuRaulMihai <94838050+CucuRaulMihai@users.noreply.github.com>
2022-10-26 13:51:32 +0300

    Lab2 DONE

---
+++ Lab2/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Shaorma n1 = new Shaorma(10, 400);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        /// vectori pot fi creati doar wrappers de la collections din Java; In general cu litera mare.
+        /// exceptia este facuta de String, ala e asa.
+        System.out.println(list);
+    }
+}
